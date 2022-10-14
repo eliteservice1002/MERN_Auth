4689d85b2c672e8f8cf93aaf93151aa4da7505b5
whitetiger1002 <nike199000@outlook.com>
2022-10-14 22:31:52 +0600

    change mongoURI

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // mongoURI: "mongodb+srv://Olexandr:R@2001S!N123qwe@mern-auth-example.5j0erxz.mongodb.net/test"
-    mongoURI:"localhost:27017/test"
+    mongoURI: "mongodb+srv://Olexandr:R@2001S!N123qwe@mern-auth-example.5j0erxz.mongodb.net/test"
+    // mongoURI:"localhost:27017/test"
   };  
